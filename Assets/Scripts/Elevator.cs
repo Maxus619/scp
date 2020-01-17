831a3277ec09ff73e3c44c2b8b08f1f2af9769c9
Maxus619 <maxus619@yandex.ru>
2020-01-18 01:03:28 +0500

    Добавил лифты (работают как телепорт, но это исправится)

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public string elevDirect;
+
+    private GameObject player;
+    
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                if (elevDirect == "up")
+                {
+                    player.transform.position += new Vector3(0, 9, 0);
+                }
+                else if (elevDirect == "down")
+                {
+                    player.transform.position += new Vector3(0, -9, 0);
+                }
+            }
+        }
+    }
+}
