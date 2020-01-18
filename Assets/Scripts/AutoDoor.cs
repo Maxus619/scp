d36fc7450c6f82fa21b92d2d5d2de8a6c17bc1ce
Maxus619 <maxus619@yandex.ru>
2020-01-18 21:38:00 +0500

    Расширил карту и добавил двери (анимация в разработке)

---
+++ Assets/Scripts/AutoDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoor : MonoBehaviour
+{
+    public string state; // opened, closed, broken (в будущем)
+
+    void Start()
+    {
+        state = "closed";
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void PressButton()
+    {
+        // TODO: Добавить проверку на поломку двери
+
+        if (state == "closed")
+        {
+            while (transform.position.y < -5.75f)
+            {
+                transform.position = Vector2.up;
+            }
+
+            state = "opened";
+        }
+        else if (state == "opened")
+        // else if (state == "opened" && transform.position.y == -5.75f) // Доп. проверка от бесконечного открытия
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * 150);
+            state = "closed";
+        }
+    }
+}
---
+++ Assets/Scripts/AutoDoorButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDoorButton : MonoBehaviour
+{
+    public AutoDoor door;
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                door.PressButton();
+            }
+        }
+    }
+}
