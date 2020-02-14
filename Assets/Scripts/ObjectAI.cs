4e4dd1d5dd5e1f221f261384ad690a2b67af6bd8
Maxus619 <maxus619@yandex.ru>
2020-02-15 03:09:35 +0500

    Added movement and LOS to SCP Object

--- Assets/Scripts/ObjectAI.cs
+++ Assets/Scripts/ObjectAI.cs
@@ -4,13 +4,74 @@ using UnityEngine;
 
 public class ObjectAI : MonoBehaviour
 {
+    [SerializeField] Transform castPoint;
+    Rigidbody2D rb;
+    Vector2 startPos;
+    float castDistance = 2f;
+
+    // Random traits
+    [SerializeField] bool canWalk;
+
+    [SerializeField] string dangerClass;
+    string[] dangerClassArr = { "safe", "euclid", "keter" };
+
+    [SerializeField] string trait;
+
+    string[] safeTraits = { "heal", "stealItem" };
+    string[] euclidTraits = { "" };
+    string[] keterTraits = { "" };
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        startPos = rb.position;
+
+        // canWalk
+        if (Random.value < 0.5)
+        {
+            canWalk = false;
+        }
+        else
+        {
+            canWalk = true;
+        }
+
+        // dangerClass
+        dangerClass = dangerClassArr[Random.Range(0, dangerClassArr.Length - 1)];
+
+        // trait
+        if (dangerClass == "safe")
+        {
+            trait = safeTraits[Random.Range(0, safeTraits.Length - 1)];
+        }
+        else if (dangerClass == "euclid")
+        {
+            trait = euclidTraits[Random.Range(0, euclidTraits.Length - 1)];
+        }
+        else
+        {
+            trait = keterTraits[Random.Range(0, keterTraits.Length - 1)];
+        }
     }
     
     void Update()
     {
-        
+        // Line of sight
+        RaycastHit2D hit = Physics2D.Raycast(castPoint.position, Vector3.right * castDistance);
+        // Debug.DrawLine(castPoint.position, Vector3.right * castDistance, Color.blue);
+
+        // Movement
+        if (hit.collider && hit.collider.CompareTag("Player"))
+        {
+            rb.velocity = new Vector2(1, 0);
+        }
+        else
+        {
+            if (rb.position.x > startPos.x)
+            {
+                rb.velocity = new Vector2(-1, 0);
+            }
+        }        
     }
 }
